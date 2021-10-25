026999e1a70160988d900eeff95b34a31cdaf19e
Daniel Braz <drz.braz@gmail.com>
2021-10-25 20:50:13 -0300

    feat(integration): add request on frontend

--- src/backend/server.js
+++ src/backend/server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 
-const routes = require('./routes/index');
+const routes = require('./src/routes/index');
 
 const server = express();
 
--- src/frontend/js/findcar.js
+++ src/frontend/js/findcar.js
@@ -1,76 +1,79 @@
-$(document).ready(function(){
+$(document).ready(function () {
+  $('#login-form').on('submit', function (e) {
+    e.preventDefault()
 
-   $("#login-form").on("submit", function(e){
-	  
-       e.preventDefault();
-	   
-	   //------Obtendo dados dos campos
-	   var modelo = $("#modelo option:selected").val();
-	   var cor = $('#cor option:selected').val();
-	   var anode = $('#anode option:selected').val();
-	   var anoate = $('#anoate option:selected').val();
-	   var km = $('#km option:selected').val();
-	   var uf = $('#uf option:selected').val();
-	   //-----end
-	   
-	   
-	   //verificar aqui anode e ate, comparar e exibir msg
-	   if(modelo == "Selecione" && cor == "Cor" &&
-		anode == "De" && anoate == "Até" && 
-		km == "Selecione" && uf == "Selecione"){
-			alert("Necessário Escolher ao menos 1 filtro para realizar a busca !","error");
-		}
-		else{
-			if(anoate != "Até"){ // verify anos
-				var convertido = parseInt(anoate);
-				if(anode != "De"){
-					var convertidoI = parseInt(anode);
-					if(convertidoI > convertido){
-						alert("Ano final deve obrigatoriamente ser maior que o ano inicial !","error");
-						location.reload();
-					}
-				}
-			}
-		  var arrayDados = {"modelo": modelo, "cor": cor, "anode": anode, "anoate": anoate, "km": km,"uf": uf}; //array com dados do form
-	   
-		   var objectAtiv = JSON.stringify(arrayDados);	//array convertido para json
-		   console.log(objectAtiv);
-		   
-		     // $.ajax({ // mandar objeto json por url
-				 // type: "POST",
-				 // data: objectAtiv,
-				 // url: "../ngrok/save",
-				 // contentType: "application/json"
-			  // }).done(function(msg) { 
-				  // console.log(msg);
-				 // //recebendo retorno
-			  // });
-			  
-			  $.getJSON("https://my-json-server.typicode.com/ErickLeal/apiFindCar/db", function(result){
-			   console.log(result);
-				var res = document.body.querySelector("#resultado"); // div p alter
-			    res.innerHTML = '';
-				var keys = Object.keys(result);
-				
-				for (key in keys){
-					var t = result[keys[key]].length;
-					for(var i = 0;i < t;i++){
-						res.innerHTML += result[keys[key]][i].marca+" ";
-						res.innerHTML += result[keys[key]][i].modelo+" ";
-						res.innerHTML += result[keys[key]][i].ano+" ";
-						res.innerHTML += result[keys[key]][i].cambio+" ";
-						res.innerHTML += result[keys[key]][i].combustivel+" ";
-						res.innerHTML += result[keys[key]][i].cor+" ";
-						res.innerHTML += result[keys[key]][i].km+" ";
-						res.innerHTML += result[keys[key]][i].portas + "<br>";
-					}
-					
-				}
-							
-			   
-			});
-		}
-	 
-	});
-});
+    //------Obtendo dados dos campos
+    const brand = $('#brand option:selected').val()
+    const color = $('#color option:selected').val()
+    const initialYear = $('#initialYear option:selected').val()
+    const finalYear = $('#finalYear option:selected').val()
+    const km = $('#km option:selected').val()
+    const state = $('#state option:selected').val()
+    //-----end
 
+    //verificar aqui initialYear e ate, comparar e exibir msg
+    if (brand == 'Selecione') {
+      alert('Necessário informar a marca do veiculo.', 'error')
+      return
+    }
+
+    if (finalYear != 'Até') {
+      // verify anos
+      const finalYearNumber = parseInt(finalYear)
+      if (initialYear != 'De') {
+        const initialYearNumber = parseInt(initialYear)
+        if (initialYearNumber > finalYearNumber) {
+          alert('Ano final deve obrigatoriamente ser maior que o ano inicial.', 'error')
+          return
+        }
+      }
+
+      const request = JSON.stringify({
+        'brand': brand,
+        'color': color,
+        'initialYear': initialYear,
+        'finalYear': finalYear,
+        'km': km,
+        'state': state
+      })
+
+      fetch('localhost:3000/api/findcar', request)
+
+      // create request
+
+      // return data to frontend
+
+      // create ad card
+
+      // $.ajax({ // mandar objeto json por url
+      // type: "POST",
+      // data: objectAtiv,
+      // url: "../ngrok/save",
+      // contentType: "application/json"
+      // }).done(function(msg) {
+      // console.log(msg);
+      // //recebendo retorno
+      // });
+
+      //   $.getJSON('https://my-json-server.typicode.com/ErickLeal/apiFindCar/db', function (result) {
+      //     console.log(result)
+      //     const res = document.body.querySelector('#resultado') // div p alter
+      //     res.innerHTML = ''
+      //     const keys = Object.keys(result)
+
+      //     for (key in keys) {
+      //       const t = result[keys[key]].length
+      //       for (const i = 0; i < t; i++) {
+      //         res.innerHTML += result[keys[key]][i].brand + ' '
+      //         res.innerHTML += result[keys[key]][i].ano + ' '
+      //         res.innerHTML += result[keys[key]][i].cambio + ' '
+      //         res.innerHTML += result[keys[key]][i].combustivel + ' '
+      //         res.innerHTML += result[keys[key]][i].color + ' '
+      //         res.innerHTML += result[keys[key]][i].km + ' '
+      //         res.innerHTML += result[keys[key]][i].portas + '<br>'
+      //       }
+      //     }
+      //   })
+    }
+  })
+})
