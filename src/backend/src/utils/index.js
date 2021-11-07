9410437bde625b854697ced453884030060edd6c
guilherme-cmd <54598542+guilherme-cmd@users.noreply.github.com>
2021-11-07 18:45:31 -0300

    Update index.js

--- src/backend/src/utils/index.js
+++ src/backend/src/utils/index.js
@@ -12,5 +12,20 @@ module.exports = {
     console.log(params);
 
     return `${url}/carro/${brand}/${model}/ano-${initialYear}-${finalYear}/preco-${initialPrice}-${finalPrice}?page=${page}&ajax`;
+  },
+  
+   createUrlAuto(url, params) {
+    const {
+      brand,
+      model,
+      initialYear,
+      finalYear,
+      initialPrice,
+      finalPrice,
+      page
+    } = params;
+
+    return `${url}/comprar/carros/novos-seminovos-usados/todos-os-estados/todas-as-cidades/${brand}/${model}/todas-as-versoes/ano-${initialYear}:${finalYear}/todas-as-cores/preco-${initialPrice}:${finalPrice}/?`;
+    //https://busca.autoline.com.br/comprar/carros/novos-seminovos-usados/todos-os-estados/todas-as-cidades/fiat/argo/todas-as-versoes/ano-2000:2022/todas-as-cores/preco-4900:100000/?
   }
 };
